466ef80252bf6732dd80af84294c7302f2fc3a20
vasanthdeveloper <125169032+vasanthdeveloper@users.noreply.github.com>
2023-04-22 16:23:44 +0530

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,11 @@
+function foo(){
+    var res=document.getElementById("firstname").value;
+    var res1=document.getElementById("middlename").value;
+    var res2=document.getElementById("lastname").value;
+    var res3=document.getElementById("email").value;
+console.log(res);
+console.log(res1);
+console.log(res2);
+console.log(res3);
+
+}
